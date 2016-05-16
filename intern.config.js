cf264ff75deead1ee74a21a939f497c2f07ce9d8
Hayato Tomoda <tomodian@gmail.com>
2016-05-17 00:34:20 +0900

    Fixed indentations

--- intern.config.js
+++ intern.config.js
@@ -1,46 +1,46 @@
 define({
-	environments: [
-		{ browserName: 'chrome' },
-	],
+  environments: [
+    { browserName: 'chrome' },
+  ],
 
-	webdriver: {
-		host: 'localhost',
-		port: 4443
-	},
+  webdriver: {
+    host: 'localhost',
+    port: 4443
+  },
 
-	maxConcurrency: 2,
+  maxConcurrency: 2,
 
-	// Name of the tunnel class to use for WebDriver tests.
-	// See <https://theintern.github.io/intern/#option-tunnel> for built-in options
+  // Name of the tunnel class to use for WebDriver tests.
+  // See <https://theintern.github.io/intern/#option-tunnel> for built-in options
   tunnel: 'NullTunnel',
 
   tunnelOptions: {
     clientUrl: 'http://localhost:4443/wd/hub',
   },
 
-	loaderOptions: {
+  loaderOptions: {
     packages: [
       { name: 'is_js', location: 'node_modules/is_js/', main: 'is' },
       { name: 'mithril', location: 'node_modules/mithril/', main: 'mithril' }
     ]
-	},
+  },
 
   basePath: './',
 
   loaders: {
   },
 
-	// Unit test suite(s) to run in each browser
-	suites: [
+  // Unit test suite(s) to run in each browser
+  suites: [
     'build/test/amd/javascripts/*-test.js',
     'build/test/amd/javascripts/**/*-test.js',
   ],
 
-	// Functional test suite(s) to execute against each browser once unit tests are completed
-	functionalSuites: [
+  // Functional test suite(s) to execute against each browser once unit tests are completed
+  functionalSuites: [
   ],
 
-	// A regular expression matching URLs to files that should not be included in code coverage analysis. Set to `true`
-	// to completely disable code coverage.
-	excludeInstrumentation: true
+  // A regular expression matching URLs to files that should not be included in code coverage analysis. Set to `true`
+  // to completely disable code coverage.
+  excludeInstrumentation: true
 });
--- intern.crossbrowser.js
+++ intern.crossbrowser.js
@@ -1,23 +1,23 @@
 define({
-	environments: [
-		{
+  environments: [
+    {
       browser_api_name: 'MblSafari8.0',
       os_api_name: 'iPhone6-iOS8sim',
       record_video: true,
       record_network: true
     },
-		{
+    {
       browser_api_name: 'MblChrome35',
       os_api_name: 'GalaxyS5-And44',
       record_video: true,
       record_network: true
     },
-	],
+  ],
 
-	maxConcurrency: 2,
+  maxConcurrency: 2,
 
-	// Name of the tunnel class to use for WebDriver tests.
-	// See <https://theintern.github.io/intern/#option-tunnel> for built-in options
+  // Name of the tunnel class to use for WebDriver tests.
+  // See <https://theintern.github.io/intern/#option-tunnel> for built-in options
   tunnel: 'NullTunnel',
 
   tunnelOptions: {
@@ -27,31 +27,31 @@ define({
     authKey: process.env.CBT_AUTHKEY
   },
 
-	loaderOptions: {
-		// Packages that should be registered with the loader in each testing environment
-		// packages: [ { name: 'dojo', location: 'lib/dojo/dojo' } ]
+  loaderOptions: {
+    // Packages that should be registered with the loader in each testing environment
+    // packages: [ { name: 'dojo', location: 'lib/dojo/dojo' } ]
     packages: [
       { name: 'is_js', location: 'node_modules/is_js/', main: 'is' },
       { name: 'mithril', location: 'node_modules/mithril/', main: 'mithril' }
     ]
-	},
+  },
 
   basePath: './',
 
   loaders: {
   },
 
-	// Unit test suite(s) to run in each browser
-	suites: [
+  // Unit test suite(s) to run in each browser
+  suites: [
     'build/test/amd/javascripts/*-test.js',
     'build/test/amd/javascripts/**/*-test.js',
   ],
 
-	// Functional test suite(s) to execute against each browser once unit tests are completed
-	functionalSuites: [
+  // Functional test suite(s) to execute against each browser once unit tests are completed
+  functionalSuites: [
   ],
 
-	// A regular expression matching URLs to files that should not be included in code coverage analysis. Set to `true`
-	// to completely disable code coverage.
-	excludeInstrumentation: true
+  // A regular expression matching URLs to files that should not be included in code coverage analysis. Set to `true`
+  // to completely disable code coverage.
+  excludeInstrumentation: true
 });
